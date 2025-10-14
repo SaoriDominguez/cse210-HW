0b1735cbd7c41073e60d9ff300c2eda62bc6bf9c
Saori Dominguez <saoridominguez1000@gmail.com>
2025-10-13 22:10:52 -0600

    W05 Inheritance: Assignment, MathAssignment, WritingAssignment, tests

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
+namespace Homework
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            //  base
+            Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine(a.GetSummary());
+
+            //  MathAssignment
+            MathAssignment m = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(m.GetSummary());
+            Console.WriteLine(m.GetHomeworkList());
+
+            // WritingAssignment
+            WritingAssignment w = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(w.GetSummary());
+            Console.WriteLine(w.GetWritingInformation());
+        }
     }
 }
